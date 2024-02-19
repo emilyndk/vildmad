be0d9ec4453745d77a0469f0ac88fd583ce6d48e
Ronja <roba0001@stud.kea.dk>
2024-02-19 14:20:46 +0100

    jssss

--- js/productlist.js
+++ js/productlist.js
@@ -1,2 +1,24 @@
+// definer hvor dataen kommer fra
 const supabaseURL = "https://yetkfhqjsmqjnluzjxmn.supabase.co";
-const supabaseKey = "";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlldGtmaHFqc21xam5sdXpqeG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3NTcwOTMsImV4cCI6MjAyMzMzMzA5M30.IG9539I2rD_1xYUORhw44Y_-8HTJDZnjL-tAzDjFQok";
+
+fetch(`${supabaseURL}/products`)
+  .then((res) => res.json())
+  .then(showProducts);
+
+function showProducts(products) {
+  // loop og kald showProduct
+  products.forEach(showProduct);
+}
+
+function showProduct(product) {
+  //fang template
+  const template = document.querySelector("#productlist_template").content;
+  // lav en kopi
+  const copy = template.cloneNode(true);
+  // ændre indhold
+
+  // appende
+  document.querySelector("main").appendChild(copy);
+}

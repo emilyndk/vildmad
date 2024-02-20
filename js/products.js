bc41e1c407db1669f293b470d6934568fd6aff48
Ronja <roba0001@stud.kea.dk>
2024-02-20 12:27:25 +0100

    fiixed singleview

--- js/products.js
+++ js/products.js
@@ -6,20 +6,20 @@ const supabaseKey =
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-fetch(
-  "https://yetkfhqjsmqjnluzjxmn.supabase.co/rest/v1/products?apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlldGtmaHFqc21xam5sdXpqeG1uIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3NTcwOTMsImV4cCI6MjAyMzMzMzA5M30.IG9539I2rD_1xYUORhw44Y_-8HTJDZnjL-tAzDjFQok" +
-    id,
-  {
-    method: "GET",
-    headers: {
-      apikey: supabaseKey,
-    },
-  }
-)
+fetch(`https://yetkfhqjsmqjnluzjxmn.supabase.co/rest/v1/products?id=eq.${id}`, {
+  method: "GET",
+  headers: {
+    apikey: supabaseKey,
+  },
+})
   .then((res) => res.json())
   .then(showProduct);
 
 function showProduct(product) {
   console.table(product);
   document.querySelector("h1").textContent = product.title;
+  document.querySelector(".product_img").src = product.product_image;
+  document.querySelector(".infolist_cat").textContent = product.categories;
+  document.querySelector(".infolist_szn").textContent = product.season;
+  document.querySelector(".infolist_loc").textContent = product.sankelandskab;
 }
